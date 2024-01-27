d2b03edde2c64ee469ed75d0dc614bbfbf4dc68d
Erik Borsos <erikborsos21@gmail.com>
2024-01-27 03:07:03 +0100

    :triangular_flag_on_post: Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from "$app/stores"
+	import { Button } from "$lib/components/ui/button"
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center">
+	<h1 class="text-6xl font-bold">{$page.status}</h1>
+	<p class="mt-5 text-2xl text-gray-600 dark:text-gray-400">{$page.error?.message}</p>
+	<Button class="mt-4" href="/">Return to Homepage</Button>
+	<div class="mt-8"></div>
+</div>
