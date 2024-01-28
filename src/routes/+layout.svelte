a646774d2631d46568d16eb4da5b1bb9a673bd88
Erik Borsos <erikborsos21@gmail.com>
2024-01-28 02:06:30 +0100

    :dizzy: Added page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,38 @@
 <script>
 	import "../app.pcss"
+	import { slide, fly } from "svelte/transition"
 	import { ModeWatcher } from "mode-watcher"
+	import { cubicIn, cubicOut, cubicInOut } from "svelte/easing"
+	import ToggleButton from "$lib/components/ToggleButton.svelte"
+
+	export let data
 </script>
 
 <ModeWatcher />
 
-<div class="flex h-screen w-full flex-col justify-center antialiased">
-	<slot />
+<div class="h-screen w-full flex-col antialiased">
+	<div class=" flex h-screen w-full flex-1 flex-col items-center justify-center gap-4">
+		{#if data.url !== "/"}
+			<div
+				transition:slide={{ duration: 100, easing: cubicInOut }}
+				class="absolute top-0 flex w-screen items-center justify-between border-b px-6 py-4"
+			>
+				<a href="/">
+					<h1 class="text-2xl font-bold text-gray-900 dark:text-white">Shorty</h1>
+				</a>
+				<nav class="flex items-center justify-center gap-4">
+					<ToggleButton />
+				</nav>
+			</div>
+		{/if}
+		{#key data.url}
+			<div
+				in:fly={{ easing: cubicOut, y: 10, duration: 200, delay: 300 }}
+				out:fly={{ easing: cubicIn, y: -10, duration: 200, delay: 100 }}
+				class="absolute flex w-full flex-1 flex-col items-center justify-center gap-4"
+			>
+				<slot />
+			</div>
+		{/key}
+	</div>
 </div>
---
+++ src/routes/shorten/+page.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button"
+	import { Input } from "$lib/components/ui/input"
+	import * as Card from "$lib/components/ui/card"
+	import * as Popover from "$lib/components/ui/popover"
+	import { slide } from "svelte/transition"
+	import { enhance } from "$app/forms"
+	import { Clipboard } from "lucide-svelte"
+	import { Label } from "$lib/components/ui/label"
+
+	let longUrl: string
+
+	export let form
+
+	const copyToClipboard = async () => {
+		if (form?.url) {
+			await navigator.clipboard.writeText(form?.url)
+			setTimeout(() => {
+				document.querySelector<HTMLElement>("[data-melt-popover-close]")!.click()
+			}, 1500)
+		}
+	}
+</script>
+
+<div class="w-full max-w-md">
+	<Card.Root>
+		<Card.Header>
+			<Card.Title class="text-3xl" tag="h2">Shorten your URL</Card.Title>
+		</Card.Header>
+		<Card.Content class="">
+			<form use:enhance method="post" class="flex flex-col gap-4 py-4 pt-4">
+				<Label for="url">
+					<Input bind:value={longUrl} id="url" name="url" placeholder="Enter your long URL here" />
+					{#if form?.missing?.url || form?.invalid?.url}
+						<p transition:slide class="text-sm text-red-500">Please enter a valid URL</p>
+					{/if}
+				</Label>
+				<Button type="submit" class="w-full">Shorten</Button>
+				{#if form?.error}
+					<p transition:slide class="text-sm text-red-500">{form?.error}</p>
+				{/if}
+			</form>
+		</Card.Content>
+	</Card.Root>
+</div>
+
+{#if form?.success}
+	<div class="w-full max-w-md" transition:slide>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title tag="h2">Your shortened URL:</Card.Title>
+			</Card.Header>
+			<Card.Content class="flex gap-2">
+				<Input readonly value={form?.url} />
+				<Popover.Root>
+					<Popover.Trigger>
+						<Button on:click={copyToClipboard} variant="outline" size="icon">
+							<Clipboard />
+						</Button>
+					</Popover.Trigger>
+					<Popover.Close />
+					<Popover.Content>URL copied to clipboard!</Popover.Content>
+				</Popover.Root>
+			</Card.Content>
+		</Card.Root>
+	</div>
+{/if}
